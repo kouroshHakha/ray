54887212853fc36ebf0d56ef642a702903f92a4c
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-04-14 17:25:37 -0700

    deprecate agents package

---
+++ rllib/agents/callbacks.py
@@ -0,0 +1,7 @@
+from ray.rllib.algorithms.callbacks import *
+from ray.rllib.utils.deprecation import deprecation_warning
+deprecation_warning(
+    'ray.rllib.agents.[...]',
+    'ray.rllib.algorithms.[...]',
+    error=False
+)
---
+++ rllib/agents/trainer_config.py
@@ -0,0 +1,7 @@
+from ray.rllib.algorithms.trainer_config import *
+from ray.rllib.utils.deprecation import deprecation_warning
+deprecation_warning(
+    'ray.rllib.agents.[...]',
+    'ray.rllib.algorithms.[...]',
+    error=False
+)
